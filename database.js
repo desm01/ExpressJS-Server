068b4833b24d574be76b2f8184d51e42c8f407eb
Desmond Madden <desmond.madden@hotmail.co.uk>
2022-06-19 20:44:01 +0100

    Using MySql2

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 import {database_creds} from "./secrets.js"
-import mysql from "mysql"
+import mysql from "mysql2"
 
-function create_connection() {
+export function create_connection() {
     const connection = mysql.createConnection({
         ...database_creds
     })
